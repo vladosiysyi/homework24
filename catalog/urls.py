e4950290c948e12d1f3cd7e06473181255241c8d
chikl <chiklow@mail.ru>
2025-05-24 17:18:14 +0300

    fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('contacts/', views.contacts_view, name='contacts'),
     path('product/<int:product_id>/', views.product_detail_view, name='product_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
