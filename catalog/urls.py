64c6e98eb018e50ddebd85d165e72eaf82c56af8
chikl <chiklow@mail.ru>
2025-06-15 16:45:12 +0300

    26.1homework

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import HomeView, ContactsView, ProductDetailView
+from .views import (
+    HomeView, ContactsView, ProductDetailView,
+    ProductListView, ProductCreateView,
+    ProductUpdateView, ProductDeleteView
+)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,58 @@
-# Create your views here.
-
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import (
+    TemplateView, ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import get_object_or_404
 from .models import Product
+from .forms import ProductForm
 
 
+# Главная страница с товарами
 class HomeView(ListView):
     model = Product
     template_name = 'home.html'
     context_object_name = 'products'
 
+
+# Страница "Контакты"
 class ContactsView(TemplateView):
     template_name = 'contacts.html'
 
 
+# Страница одного товара
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'product_id'
+
+
+# CRUD: список товаров
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+
+
+# CRUD: создание товара
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+
+# CRUD: обновление товара
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+
+# CRUD: удаление товара
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
